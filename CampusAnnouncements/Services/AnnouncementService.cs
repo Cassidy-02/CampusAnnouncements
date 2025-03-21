ef70213dd4b70e3b577a71396a38ed139fac5521
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-03-21 09:50:12 +0200

    -AnnoucementServices stores the List of Announcements and when a new announcement is created on the web application it is stored in the List as well.

---
+++ CampusAnnouncements/Services/AnnouncementService.cs
@@ -0,0 +1,29 @@
+﻿using CampusAnnouncements.Models;
+
+namespace CampusAnnouncements.Services
+{
+    public class AnnouncementService
+    {
+        private readonly List<Announcement> _announcements;
+
+        public AnnouncementService()
+        {
+            _announcements = new List<Announcement>
+            {
+                new Announcement { Id = 1, Title = "Welcome Week", Content = "Join us for a week of fun activities!", Date = DateTime.Now.AddDays(-1) },
+                new Announcement { Id = 2, Title = "Guest Lecture", Content = "Don't miss the guest lecture by Dr. Smith on AI.", Date = DateTime.Now }
+            };
+        }
+
+        public List<Announcement> GetAnnouncements()
+        {
+            return _announcements.OrderByDescending(a => a.Date).ToList();
+        }
+
+        public void AddAnnouncement(Announcement announcement)
+        {
+            announcement.Id = _announcements.Max(a => a.Id) + 1;
+            _announcements.Add(announcement);
+        }
+    }
+}
